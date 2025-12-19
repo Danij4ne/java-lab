2051b17e1f51c6ce5d44a2425a3c4b95fd78ae03
Daniel Jane <danij4ne@gmail.com>
2025-12-19 13:32:49 +0100

    add explanation of getters/setters and access modifiers

--- oop/classes_objects/Classes.java
+++ oop/classes_objects/Classes.java
@@ -4,17 +4,17 @@ public class Classes {
     public static void main(String[] args) {
 
         // Create a Person object using the constructor and initialize its attributes
-        var person = new Person("Dani", 24);
+        var person = new Person("Dani", 24, 12);
 
         // Assign values to the object's fields
-        person.name = "Dani";
-        person.age = 24;
+        //person.name = "Dani";  Name and age are now private, so they cannot be modified directly.
+        //person.age = 24;
 
         // Call the method to display a greeting
         person.sayHello();
 
         // Create another Person object with different attribute values
-        var person2 = new Person("Daniel", 23);
+        var person2 = new Person("Daniel", 23, 2);
 
         // Call the method for the second object, using its own data
         person2.sayHello();
--- oop/classes_objects/Person.java
+++ oop/classes_objects/Person.java
@@ -3,19 +3,20 @@ package oop.classes_objects;
 public class Person {
 
     // Attributes (fields)
-    String name; // stores the person's name
-    int age;     // stores the person's age
-
+    private String name; // stores the person's name
+    final private int id ;     // Stores the person's id. Being declared as 'final', its value can only be assigned once (typically in the constructor) and cannot be modified afterwards
+    private int age ;                        // neither from outside nor within the class.
     // Constructor
-    public Person(String name, int age) {
+    public Person(String name, int age, int id) {
         // The constructor forces the object to be created with a name and an age (not optional)
 
         // 'this.name' refers to the class attribute,
         // while 'name' refers to the constructor parameter
         this.name = name;
-
+        this.id = id ;
         // Assign the constructor parameter to the class attribute
-        this.age = age;
+        this.setAge(age) ; // Assigns the age to the 'age' attribute using the setter to apply validation before storing it
+
     }
 
     // Methods
@@ -23,4 +24,32 @@ public class Person {
         // Calls the method to display a greeting using the object's data
         System.out.print("Hi, I am " + name + " and I am " + age + " years old");
     }
+
+ // Getters / Setters  -> since int and String are private, we create methods to access their values
+
+// Getters
+
+    public int getAge(){  // example of a getter to retrieve and print the age
+        return age;
+    }
+
+
+// Setter
+
+    public void setAge(int age){ // validates that the value is greater than 0 before using the setter to modify the field
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.print("Invalid age");
+        }
+    }
+
+
+
+// Access Modifiers
+// public    -> can be accessed from anywhere in the program
+// private   -> can only be accessed from within the class
+// protected -> can be accessed within the same package and by subclasses
+// default   -> allows access only within the same package
+
 }
